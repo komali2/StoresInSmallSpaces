48bb09168b9dcdf2273f1064a3dc27d746984594
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-04 12:58:22 -0700

    core: built out middleware, need to install bodyparser

---
+++ server/config/middleware.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var bodyParser = require('body-parser');
+
+module.exports = function(app, express){
+  var homeRouter = express.Router();
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  app.use(express.static(__dirname + '/../../client'));
+  app.use('/home', homeRouter);
+
+  require('../home/HomeRoutes.js')(homeRouter);
+}
