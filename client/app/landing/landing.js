b786cca5ca78e1350e6f5712bd8c2916414d9f77
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-08 11:15:21 -0700

    bug: fixed typeerrors and missing script tags causing dependency error

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -1,8 +1,8 @@
 var app = angular.module('storiesApp.landing', []);
 
-app.service('landingService', function($http, $location)){
+app.service('landingService', function($http, $location){
 
-};
+});
 
 app.controller('landingController', function($scope, $location){
 
