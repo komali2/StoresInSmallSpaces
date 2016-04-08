d4ad850a0b1bd3c23fc774f7f6fc9a3a7cf1bc18
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-08 11:04:37 -0700

    core: built out framework of landing.js

---
+++ client/app/landing/landing.js
@@ -0,0 +1,9 @@
+var app = angular.module('storiesApp.landing', []);
+
+app.service('landingService', function($http, $location)){
+
+};
+
+app.controller('landingController', function($scope, $location){
+
+});
