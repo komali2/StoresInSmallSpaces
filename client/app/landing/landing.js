b75ee6374ab91cc3eebcbce97ec0fc84f0504be3
komali2 <rogersjcaleb@gmail.com>
2016-04-10 14:06:24 -0700

    core: built out landing service

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -1,9 +1,23 @@
 var app = angular.module('storiesApp.landing', []);
 
 app.service('landingService', function($http, $location){
+  this.submit = function(value){
+    return $http({
+      method: 'POST',
+      url: '/home/search',
+      data: value
+    })
+    .then(function success(res){
+      return res.data;
+    }), function (error(res){
+      return res;
+    });
+  };
 
 });
 
 app.controller('landingController', function($scope, $location){
-
+  $scope.submit = function(){
+    landingService.submit
+  }
 });
