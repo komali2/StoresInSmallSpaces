645c72fc23429bc077f1315250886237923773b0
komali2 <rogersjcaleb@gmail.com>
2016-04-10 16:03:53 -0700

    feat: server can now search to see if zip exists in false DB

--- server/home/homeController.js
+++ server/home/homeController.js
@@ -9,16 +9,10 @@ module.exports = {
 }
   */
   search: function(req, res){
-    console.log('called search');
     var found = false;
-    console.log(req.body.zip);
     for(var i = 0; i < db.stories.length; i++){
-      console.log(db.stories[i].zip);
-      console.log(db.stories[i].zip === req.body.zip);
       if(Number(db.stories[i].zip) === Number(req.body.zip)){
-        console.log('found zip', db.stories[i].zip);
         found = true;
-        console.log(db.stories[i].zip);
       }
     }
     res.send(found);
