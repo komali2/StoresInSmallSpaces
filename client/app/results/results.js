c8dcf73fb8d8c0af741bc52f7c5cbc6d17762933
komali2 <rogersjcaleb@gmail.com>
2016-04-10 20:16:29 -0700

    core: results.js showResults function skeleton built out

--- client/app/results/results.js
+++ client/app/results/results.js
@@ -1,5 +1,13 @@
 var app = angular.module('storiesApp.results', []);
 
-app.service('resultsService', function(){});
+app.service('resultsService', function(){
+  this.showResults = function(cb){
+    cb(JSON.parse(localStorage.getItem('stories')));
+  }
+});
 
-app.controller('resultsController', function($scope, resultsService){});
+app.controller('resultsController', function($scope, resultsService){
+  $scope.stories = resultsService.showResults(function(stories){
+    return stories;
+  });
+});
